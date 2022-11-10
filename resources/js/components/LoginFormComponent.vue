b651c9cd83a320c468ab64cc0cce61b78e57acec
Anton Kerbel <anton.kerbel@chairish.com>
2022-11-10 19:52:11 +0100

    Vue: beginning

---
+++ resources/js/components/LoginFormComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <form class="login-form" @submit.prevent="loginButtonPressed">
+        <h4>Login</h4>
+        <label for="email">Login: <input name="email" type="email" placeholder="Email"></label>
+        <label for="password">Password: <input name="password" type="password" placeholder="Password"></label>
+        <button class="login">Login</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {
+              token: ''
+          }
+        },
+        methods: {
+            loginButtonPressed(e) {
+                alert('test!');
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
